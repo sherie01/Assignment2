4ce734a7654b5a12524a3e6587993c42808624e7
sherie01 <159250295+sherie01@users.noreply.github.com>
2024-10-22 03:30:18 +0800

    dev-log: Fixed JSX closing tag in Modal.js by adding '/' to input elements. (#1)

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import "./header.css";
 export default function Header() {
   return (
     <div className="Header">
-      <img src="image/grocery.png" />
+      <img src="image/grocery.png" alt="Grocery icon" />
       <h2>Your Grocery List</h2>
     </div>
   );
--- src/Modal.js
+++ src/Modal.js
@@ -6,21 +6,21 @@ export default function Modal() {
         <div class="modal-content">
             <span class="close" onclick="closeAddItemModal()">&times;</span>
             <h2>Add Grocery Item</h2>
-                <form id="add-item-form">
-                        <input type="text" id="product-name" placeholder="Product Name" required>
-                        <input type="text" id="brand" placeholder="Brand" required>
-                        <input type="number" id="price" placeholder="Price" required>
-                        <input type="text" id="weight" placeholder="Weight/Volume" required>
-                        <input type="number" id="quantity" placeholder="Quantity" required>
-                        <input type="text" id="store" placeholder="Store" required>
-                        <input type="file" id="image" accept="image/*">
-                        <select id="category">
-                            <option value="Fruit">Fruit</option>
-                            <option value="Vegetable">Vegetable</option>
-                            <option value="Dairy">Dairy</option>
-                        </select>
-                        <button type="submit">Add Item</button>
-                </form>
+            <form id="add-item-form">
+                <input type="text" id="product-name" placeholder="Product Name" required />
+                <input type="text" id="brand" placeholder="Brand" required />
+                <input type="number" id="price" placeholder="Price" required />
+                <input type="text" id="weight" placeholder="Weight/Volume" required />
+                <input type="number" id="quantity" placeholder="Quantity" required />
+                <input type="text" id="store" placeholder="Store" required />
+                <input type="file" id="image" accept="image/*" />
+                <select id="category">
+                    <option value="Fruit">Fruit</option>
+                    <option value="Vegetable">Vegetable</option>
+                    <option value="Dairy">Dairy</option>
+                </select>
+                <button type="submit">Add Item</button>
+            </form>
         </div>
     </div>
   );
