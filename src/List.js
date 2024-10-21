c48ca8aee575bd031a9ac5d28115cf3a083e6efc
sherie01 <sheriedarlene.barila.138531@gmail.com>
2024-10-22 03:04:22 +0800

    dev-log: Fixed JSX closing tag in Modal.js by adding '/' to input elements.

--- src/List.js
+++ src/List.js
@@ -1,12 +1,13 @@
 import "./list.css";
 
 export default function List(props) {
-    return (
-      <div className="List">
-       <img src={props.Img} />
-       <p>{props.productName}</p>
-       <button id="btn-1"> Edit </button>
-       <button id="btn-2"> Remove </button>
-      </div>
-    );
-  }
+  return (
+    <div className="List">
+      <img src={props.Img} alt={props.productName} />
+      <p>{props.productName}</p>
+      <button id="btn-1"> Edit </button>
+      <button id="btn-2"> Remove </button>
+    </div>
+  );
+}
+
